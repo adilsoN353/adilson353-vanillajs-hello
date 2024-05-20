11c62dbcb8d019a3a028026a94f88186b026dbda
adilson aguilar <163350523+adilsoN353@users.noreply.github.com>
2024-05-20 10:44:16 +0000

    generadorDeDominos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  function generateDomains(pronoun, adj, noun, extensions) {
+    let domains = [];
+    for (let primero of pronoun) {
+      for (let segundo of adj) {
+        for (let tercero of noun) {
+          for (let ext of extensions) {
+            let domain = primero + segundo + tercero + ext;
+           console.log(domain);
+          }
+        }
+      }
+    }
+    return domains;
+  }
+
+  let generatedDomains = generateDomains(pronoun, adj, noun, extensions);
+  console.log(generatedDomains);
+
+  generatedDomains.forEach(function(domain) {
+    console.log(domain);
+  });
+
